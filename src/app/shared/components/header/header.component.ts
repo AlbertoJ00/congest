e48482e2a1df1780c1be1cf6657f7cb9b83d1f1f
Maikol026 <maikolheredia40@gmail.com>
2026-08-04 16:07:50 -0400

    Update header.component.ts

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -85,7 +85,7 @@ export class HeaderComponent {
     }
 
     if (this.currentUser.rol === 'Inquilino') {
-      return this.sections.filter(section => ['Dashboard', 'Condominios'].includes(section.name));
+      return this.sections.filter(section => ['Dashboard'].includes(section.name));
     }
 
     return this.sections.filter(section => ['Dashboard', 'Condominios', 'Inquilinos', 'Reportes', 'Pagos', 'Incidencias', 'Estados de Cuenta'].includes(section.name));
